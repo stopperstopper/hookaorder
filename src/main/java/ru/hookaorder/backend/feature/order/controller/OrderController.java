72f8d20dfbc32210abfb20944e22587603481940
Alex Sadykov <92750261+stopperstopper@users.noreply.github.com>
2022-09-01 10:41:31 +0500

    swagger v1

--- src/main/java/ru/hookaorder/backend/feature/order/controller/OrderController.java
+++ src/main/java/ru/hookaorder/backend/feature/order/controller/OrderController.java
@@ -1,6 +1,8 @@
 package ru.hookaorder.backend.feature.order.controller;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,27 +13,32 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/order")
+@Api(description = "Контроллер заказов")
 public class OrderController {
     @Autowired
     private OrderRepository orderRepository;
 
     @GetMapping("/get/{id}")
+    @ApiOperation("Получение заказа по id")
     ResponseEntity<OrderEntity> getPlaceById(@PathVariable Long id) {
         return orderRepository.findById(id).map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @GetMapping("/get/all")
+    @ApiOperation("Получение всех заказов")
     ResponseEntity<List<OrderEntity>> getAllPlaces() {
         return ResponseEntity.ok(orderRepository.findAll());
     }
 
     @PostMapping("/create")
+    @ApiOperation("Создание заказа")
     ResponseEntity<OrderEntity> createPlace(@RequestBody OrderEntity orderEntity) {
         return ResponseEntity.ok(orderRepository.save(orderEntity));
     }
 
     @PostMapping("/update")
+    @ApiOperation("Обновление заказа по id")
     ResponseEntity<OrderEntity> updatePlace(@PathVariable Long id) {
         return orderRepository.findById(id).map((val) -> ResponseEntity.ok(orderRepository.save(val)))
                 .orElse(ResponseEntity.badRequest().build());

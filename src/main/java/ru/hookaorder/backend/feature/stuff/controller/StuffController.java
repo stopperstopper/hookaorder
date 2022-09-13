c9ab8e874872e5d94c26a316054cc7ef5ca74415
Miroshinsv <miroshinsv1988@gmail.com>
2022-09-13 21:09:45 +0300

    add check users exist on add stuff

--- src/main/java/ru/hookaorder/backend/feature/stuff/controller/StuffController.java
+++ src/main/java/ru/hookaorder/backend/feature/stuff/controller/StuffController.java
@@ -1,12 +1,13 @@
 package ru.hookaorder.backend.feature.stuff.controller;
 
+import com.google.common.collect.Sets;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import ru.hookaorder.backend.feature.place.repository.PlaceRepository;
-import ru.hookaorder.backend.feature.user.entity.UserEntity;
+import ru.hookaorder.backend.feature.user.repository.UserRepository;
 import ru.hookaorder.backend.utils.CheckOwnerAndRolesAccess;
 
 import java.util.Set;
@@ -16,13 +17,14 @@ import java.util.Set;
 @AllArgsConstructor
 public class StuffController {
     private final PlaceRepository placeRepository;
+    private final UserRepository userRepository;
 
     @PreAuthorize("hasAnyAuthority('ADMIN', 'OWNER')")
     @PostMapping("/stuff/add/{id}")
-    public ResponseEntity<?> addStuffToPlace(@PathVariable Long id, @RequestBody Set<UserEntity> users, Authentication authentication) {
+    public ResponseEntity<?> addStuffToPlace(@PathVariable Long id, @RequestBody Set<Long> users, Authentication authentication) {
         return placeRepository.findById(id).map((val) -> {
             if (CheckOwnerAndRolesAccess.isOwnerOrAdmin(val, authentication)) {
-                val.setStuff(users);
+                val.setStuff(Sets.newHashSet(userRepository.findAllById(users)));
                 return ResponseEntity.ok().body(placeRepository.save(val));
             }
             return ResponseEntity.badRequest().body("Access denied");
@@ -31,10 +33,10 @@ public class StuffController {
 
     @PreAuthorize("hasAnyAuthority('ADMIN', 'OWNER')")
     @PostMapping("/stuff/delete/{id}")
-    public ResponseEntity<?> deleteStuffFromPlace(@PathVariable Long id, @RequestBody Set<UserEntity> users, Authentication authentication) {
+    public ResponseEntity<?> deleteStuffFromPlace(@PathVariable Long id, @RequestBody Set<Long> users, Authentication authentication) {
         return placeRepository.findById(id).map((val) -> {
             if (CheckOwnerAndRolesAccess.isOwnerOrAdmin(val, authentication)) {
-                val.getStuff().removeAll(users);
+                val.getStuff().removeAll(Sets.newHashSet(userRepository.findAllById(users)));
                 return ResponseEntity.ok().body(placeRepository.save(val));
             }
             return ResponseEntity.badRequest().body("Access denied");

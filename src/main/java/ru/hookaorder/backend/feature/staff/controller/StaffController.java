72e9ba5ee878ff23b297bfbbd108e3590e204753
Miroshinsv <miroshinsv1988@gmail.com>
2022-09-14 21:18:30 +0300

    fix methods name, change id to placeId

--- src/main/java/ru/hookaorder/backend/feature/staff/controller/StaffController.java
+++ src/main/java/ru/hookaorder/backend/feature/staff/controller/StaffController.java
@@ -1,4 +1,4 @@
-package ru.hookaorder.backend.feature.stаff.controller;
+package ru.hookaorder.backend.feature.staff.controller;
 
 import com.google.common.collect.Sets;
 import lombok.AllArgsConstructor;
@@ -20,11 +20,11 @@ public class StaffController {
     private final UserRepository userRepository;
 
     @PreAuthorize("hasAnyAuthority('ADMIN', 'OWNER')")
-    @PostMapping("/stuff/add/{id}")
-    public ResponseEntity<?> addStuffToPlace(@PathVariable Long id, @RequestBody Set<Long> users, Authentication authentication) {
-        return placeRepository.findById(id).map((val) -> {
+    @PostMapping("/stuff/add/{placeId}")
+    public ResponseEntity<?> addStaffToPlace(@PathVariable Long placeId, @RequestBody Set<Long> users, Authentication authentication) {
+        return placeRepository.findById(placeId).map((val) -> {
             if (CheckOwnerAndRolesAccess.isOwnerOrAdmin(val, authentication)) {
-                val.setStuff(Sets.newHashSet(userRepository.findAllById(users)));
+                val.setStaff(Sets.newHashSet(userRepository.findAllById(users)));
                 return ResponseEntity.ok().body(placeRepository.save(val));
             }
             return ResponseEntity.badRequest().body("Access denied");
@@ -32,11 +32,11 @@ public class StaffController {
     }
 
     @PreAuthorize("hasAnyAuthority('ADMIN', 'OWNER')")
-    @PostMapping("/stuff/delete/{id}")
+    @PostMapping("/staff/delete/{id}")
     public ResponseEntity<?> deleteStuffFromPlace(@PathVariable Long id, @RequestBody Set<Long> users, Authentication authentication) {
         return placeRepository.findById(id).map((val) -> {
             if (CheckOwnerAndRolesAccess.isOwnerOrAdmin(val, authentication)) {
-                val.getStuff().removeAll(Sets.newHashSet(userRepository.findAllById(users)));
+                val.getStaff().removeAll(Sets.newHashSet(userRepository.findAllById(users)));
                 return ResponseEntity.ok().body(placeRepository.save(val));
             }
             return ResponseEntity.badRequest().body("Access denied");
